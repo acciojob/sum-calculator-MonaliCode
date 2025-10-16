601b505954e161e254f7bf320a0099640f4cc40a
Monali Gupta <monaligupta06@gmail.com>
2025-10-16 18:57:17 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,29 +2,24 @@ import React, { useState, useEffect } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [num, setNum] = useState();
-  const [sum, setSum] = useState();
+  const [sum, setSum] = useState(0);
+  const [curr, setCurr] = useState(0);
 
   useEffect(() => {
-    if (num > 0) {
-      setSum((num * (num + 1)) / 2);
-    } else {
-      setSum(0);
-    }
-  }, [num]);
+    setSum(sum + Number(curr))
+  }, [curr]);
 
   return (
     <div>
       <h1>Sum Calculator</h1>
-      <input
-        value={num}
-        onChange={(e) => {
-          const value = parseInt(e.target.value);
-          setNum(isNaN(value) ? 0 : value);
-        }}
+      <input type = "number"
+        onChange = {(e) => {
+            setCurr(e.target.value)
+        }
+      }
         type="number"
       />
-      <h1>Sum: {sum}</h1>
+      <p>Sum: {sum}</p>
       {/* <hr className="border border-gray-400" /> */}
     </div>
   );
